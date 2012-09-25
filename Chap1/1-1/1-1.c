3b4207d5a269809e3e6b940ca97f8c9c05405a02
vlsi1217 <vlsi1217@gmail.com>
2012-09-25 13:18:32 -0700

    The simple hello Github C file.
    one thing to say is to add:
    
    system("pause");
    return 0;
    
    after the printf so as to tell the system 
    to pause when the popup window shows up.

--- Chap1/1-1/1-1.c
+++ Chap1/1-1/1-1.c
@@ -2,6 +2,7 @@
 
 void main(){
 	printf("hello GitHub\n");
-	
+	system("pause"); //inorder to have see the result by hitting the run
+			//need to add a system pause so as to pause the popup window
 	return 0;
 }
