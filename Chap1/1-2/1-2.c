32dae075f74c0eb3a491d5e96d58c1737440d603
vlsi1217 <vlsi1217@gmail.com>
2012-09-27 00:50:29 -0700

    Learn the use of loop function
    Everything you done out side the loop will not affect the value of
    inside loop function. the 'F' is a good example to exhibit this idea.

--- Chap1/1-2/1-2.c
+++ Chap1/1-2/1-2.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+
+main(){
+       int C, F;
+       C = 60;
+       F = 5*(C-32)/9;   /*If I define F here, then the 
+                         F is locked as 5(60-31)/9, a 
+                         constant to print out, because 
+                         the printf function can print F
+                         and the C is changing. */
+       while (C<200)
+             {//F = 5*(C-32)/9; 
+             /*if I define F in the while loop, the prinf
+             can print Updated F every time. Know new stuff
+             everyday. SO CS is convenient to learn by 
+             Changing the place you put a line of code*/
+             printf("%d %d\n", C, F);
+             C+=10;
+             }
+       system("pause");
+       return 0;      
+       }
