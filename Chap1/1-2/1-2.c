0ca98363d3775c8078964ea0ab65afffe485c3d6
vlsi1217 <vlsi1217@gmail.com>
2012-09-27 01:04:20 -0700

    Another found of F inside or outside loop
    I set F = 10 and F = f(C) inside loop. the printf() works for F inside
    the loop.

--- Chap1/1-2/1-2.c
+++ Chap1/1-2/1-2.c
@@ -3,13 +3,13 @@
 main(){
        int C, F;
        C = 60;
-       F = 5*(C-32)/9;   /*If I define F here, then the 
-                         F is locked as 5(60-31)/9, a 
-                         constant to print out, because 
-                         the printf function can print F
-                         and the C is changing. */
+       F = 10;   /*If I define F here, then the 
+                         F is locked as 10, a 
+                         constant, because the printf function will 
+                         print F(the one inside loop) and 
+                         the C is changing. */
        while (C<200)
-             {//F = 5*(C-32)/9; 
+             {F = 1*(C-32)/9; 
              /*if I define F in the while loop, the prinf
              can print Updated F every time. Know new stuff
              everyday. SO CS is convenient to learn by 
